f5215262dce2e5517f6e6c7fb1beceb3409bd7de
Abdullah-FSWD <ansarimdabdullah794@gmail.com>
2023-12-01 15:12:21 +0530

    created the portfolio section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -17,3 +17,17 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
